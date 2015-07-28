fb43eeba8b630a16ae169c6a381914afbaf8178f
root <root@localhost.localdomain>
2015-07-28 00:40:00 +0000

    Added params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,18 @@
 class tor::params {
+  $ensure		= running
   $socks_port 		= '9050'
   $deamon 		= false
   $data_dir 		= '/var/lib/tor'
   $use_dns_port 	= false
-  
+  $use_control_port     = false 
+  $or_port		= '9001'
+  $nickname		= 'nickname'
+  $exit_policy		= 'reject'
+ 
   if $use_dns_port {
     $dns_port 		= '0'
   }
 
-  $use_control_port 	= false
   if $use_control_port {
     $control_port 	= '9051'
   }
