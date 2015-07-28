78e46c97c854ee5070e358f90f785861d49bd967
root <root@localhost.localdomain>
2015-07-28 00:39:31 +0000

    changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: tor
 #
-# Full description of class tor here.
+# This module is for setting up Tor anonymous proxy as a client, relay or as an exit node.
 #
 # === Parameters
 #
@@ -22,19 +22,50 @@
 #   of Puppet 2.6.)
 #
 # Sample Usage:
-#   class { 'tor': }
+#   class { '::tor': }
+#
+# For using Tor as client:
+#    class { '::tor::client': }
+#
+# For using Tor as relay:
+#    class { '::tor::relay': }  
 
 class tor(
-  $socks_port 	= $tor::params::socks_port,
-  $daemon 	= $tor::params::daemon,
-  $data_dir 	= $tor::params::data_dir,
-  $dns_port 	= $tor::params::dns_port,
-) inherits tor::params {
+  $ensure = $tor::params::ensure,
+)
+ inherits tor::params {
+
+  class {'::tor::repos':}
+
   package {'tor':
-    ensure => present,
+    ensure  => present,
+    require => Yumrepo['Tor-repo'],
   }
-  
-  file {$tor::params::conf:
+
+  service {'tor':
+    ensure    => $ensure,   
+    require   => Package['tor'],
+  }
+}
+
+class tor::client(
+  $ensure 		= $::tor::params::ensure,
+  $socks_port           = $::tor::params::socks_port,
+  $daemon               = $::tor::params::daemon,
+  $data_dir             = $::tor::params::data_dir,
+  $use_dns_port         = $::tor::params::use_dns_port,
+  $dns_port             = $::tor::params::dns_port,
+  $use_control_port     = $::tor::params::use_control_port,
+  $control_port         = $::tor::params::control_port,
+) inherits tor::params {
+  validate_bool($use_dns_port)
+  validate_bool($use_control_port)
+
+  notify{'Setting up Tor client...':}
+  class{'::tor':
+    ensure	=> $ensure,
+  }
+  file {$::tor::params::conf:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
@@ -43,9 +74,40 @@ class tor(
     require => Package['tor'],
     notify  => Service['tor'],
   }
+}
 
-  service {'tor':
-    ensure => running, 
+class tor::relay(
+  $ensure               = $::tor::params::ensure,
+  $or_port		= $::tor::params::or_port,
+  $nickname		= $::tor::params::nickname,
+  $exit_policy		= $::tor::params::exit_policy,
+) inherits tor::params {
+    notify{'Setting up Tor relay...':}
+    class{'::tor':
+      ensure      => $ensure,
+    }
+    $tor_relay = true
+
+    unless $exit_policy =~ Enum['accept', 'reject'] {
+      fail("exit_policy must be either accept or reject! Value passed was: $exit_policy")
+    }
+
+    file {$::tor::params::conf:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('tor/torrc.erb'),
+      require => Package['tor'],
+      notify  => Service['tor'],
+    }
+}
+
+class tor::exit_node(
+  $ensure               = $::tor::params::ensure,
+  $or_port              = $::tor::params::or_port,
+) inherits tor::params {
+  class{'::tor':
+    ensure      => $ensure,
   }
-
 }
